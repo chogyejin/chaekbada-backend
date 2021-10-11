c340c42720ae514c89a49bca0364767cff89db31
GWLOCAL\32174918 <beewt@naver.com>
2021-10-11 20:28:04 +0900

    db 명 변경

--- src/constant.ts
+++ src/constant.ts
@@ -2,8 +2,8 @@ const SERVER_PORT = 3000;
 
 const DB_PORT = 5432;
 const DB_HOST = '127.0.0.1';
-const DB_USER = 'chaeckbada';
+const DB_USER = 'chaekbada';
 const DB_PASSWORD = '1234';
-const DB_NAME = 'chaeckbada';
+const DB_NAME = 'chaekbada';
 
 export { DB_PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, SERVER_PORT };
