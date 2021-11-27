e9319c6ae35aa90988f14aaf1e5542e50205cd1a
yatyat2 <xowls2287@naver.com>
2021-11-27 17:12:54 +0900

    production과 dev 사이에 다른 설정 값 수정

--- src/constant.ts
+++ src/constant.ts
@@ -1,16 +1,16 @@
 export const IS_PRODUCTION =
   process.env.NODE_ENV === 'production' ? true : false;
 
-const SERVER_PORT = IS_PRODUCTION ? '' : 4000;
+const SERVER_PORT = IS_PRODUCTION ? 8081 : 4000;
 
-const DB_PORT = IS_PRODUCTION ? '' : 5432;
-const DB_HOST = IS_PRODUCTION ? '' : '127.0.0.1';
+const DB_PORT = 5432;
+const DB_HOST = IS_PRODUCTION ? 'chaekbada.cfgblzfndycn.ap-northeast-2.rds.amazonaws.com' : '127.0.0.1';
 const DB_USER = 'chaekbada';
-const DB_PASSWORD = '1234';
+const DB_PASSWORD = IS_PRODUCTION ? "12341234":'1234';
 const DB_NAME = 'chaekbada';
 
 const SENDER_MAIL = 'rezzo@dkuh.co.kr';
-const FRONT_END_URL = IS_PRODUCTION ? '' : 'http://localhost:3000';
+const FRONT_END_URL = IS_PRODUCTION ? 'Chaekbadauser-env.eba-6etkpnv3.ap-northeast-2.elasticbeanstalk.com' : 'http://localhost:3000';
 const TEST_RECEIVER_EMAIL = 'beewt@naver.com';
 const TOASTS_APP_KEY = 'zcClkQzLxt0Pf3Oh';
 const TOASTS_SECRET_KEY = 'Fq7Qh1iV';
